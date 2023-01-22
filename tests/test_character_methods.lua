1a5ecd6bacd185a1aada3277f931dc3220ecd7a9
datagutten <datagutten@datagutten.net>
2023-01-22 20:48:48 +0100

    Add tests for CharacterData methods

---
+++ tests/test_character_methods.lua
@@ -0,0 +1,65 @@
+local lu = require('luaunit')
+loadfile('build_utils/wow_api/functions.lua')()
+loadfile('build_utils/wow_api/frame.lua')()
+loadfile('build_utils/wow_api/mixin.lua')()
+loadfile('build_utils/wow_api/Color.lua')()
+loadfile('build_utils/wow_api/constants.lua')()
+
+if _G.WOW_PROJECT_ID == _G.WOW_PROJECT_CLASSIC then
+    loadfile('build_utils/wow_api/container_classic.lua')()
+else
+    loadfile('build_utils/wow_api/container.lua')()
+end
+loadfile('build_utils/utils/load_toc.lua')('../LibInventory.toc')
+---@type LibInventory
+local addon = _G['AddonTable']
+
+_G['Characters'] = {
+    ["Mirage Raceway"] = {
+        Quadduo = {
+            class = "HUNTER",
+            classFilename = "HUNTER",
+            classId = 3,
+            className = "Hunter",
+            gender = 3,
+            guild = "The Wasnots",
+            money = 62993,
+            name = "Quadduo",
+            race = "NightElf",
+            raceFile = "NightElf",
+            raceID = 4,
+            raceName = "Night Elf",
+            realm = "Mirage Raceway"
+        }
+    }
+}
+
+---@type CharacterData
+local character_obj = addon.characters.characterInfo('Mirage Raceway', 'Quadduo')
+
+function testCharacterProperties()
+    lu.assertEquals(character_obj.money, 62993)
+    lu.assertEquals(character_obj.guild, 'The Wasnots')
+    lu.assertEquals(character_obj.name, 'Quadduo')
+    lu.assertEquals(character_obj.realm, 'Mirage Raceway')
+end
+
+function testCharacterColor()
+    lu.assertEquals(character_obj:color():GenerateHexColor(), 'ffaad372')
+end
+
+function testCharacterGenderString()
+    lu.assertEquals(character_obj:genderString(), 'female')
+end
+
+function testCharacterIcon()
+    local _, icon = character_obj:icon()
+    lu.assertEquals(icon, { 0.75, 1.0, 0.5, 0.75 })
+end
+
+function testCharacterIconString()
+    lu.assertEquals(character_obj:iconString(),
+            "|TInterface/Glues/CharacterCreate/UI-CharacterCreate-Races:12:12:0:0:128:128:96:128:64:96|t")
+end
+
+os.exit(lu.LuaUnit.run())
