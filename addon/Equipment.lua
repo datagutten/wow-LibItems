9ef5cc22ebcaed35ba33de9497a1204a96e4673b
datagutten <datagutten@datagutten.net>
2023-08-20 10:14:35 +0200

    Rewrite equipment scanner

--- addon/Equipment.lua
+++ addon/Equipment.lua
@@ -1,23 +1,32 @@
----@type LibInventory
-local _, addon = ...
-if not addon.equipment then
-    return
-end
+---@type LibInventoryAce
+local addon = _G['LibInventoryAce']
 
 ---@class LibInventoryEquipment
-local lib = {}
-addon.equipment = lib
-lib.addon = addon
+local lib = addon:NewModule('LibInventoryEquipment', 'AceEvent-3.0')
+
+---@type BMUtils
+local utils = _G.LibStub('BM-utils-2')
+
+---@type LibInventoryLocations
+local inventory = addon:GetModule('LibInventoryLocations')
+
+function lib:OnEnable()
+    self:RegisterEvent('PLAYER_EQUIPMENT_CHANGED')
+end
 
 function lib:scanEquipment()
     --https://www.townlong-yak.com/framexml/9.0.2/Constants.lua#192
     self.addon.main:clearLocation('equipment')
     local link, itemID
     for slot = 0, 19 do
-        link = _G.GetInventoryItemLink("player", slot)
+        link = _G.GetInventoryItemLink('player', slot)
         if link ~= nil then
-            itemID = self.addon.utils.itemIdFromLink(link)
-            self.addon.main:saveItemLocation(itemID, 'equipment', 1)
+            itemID = utils.itemIdFromLink(link)
+            inventory:saveItemLocation(itemID, 'equipment ', 1)
         end
     end
+end
+
+function lib:PLAYER_EQUIPMENT_CHANGED()
+    self:scanEquipment()
 end
