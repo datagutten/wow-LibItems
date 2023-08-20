a6b3f0864ccd3f7d5977ccb7a9bc1c9b3f3bd2f3
datagutten <datagutten@datagutten.net>
2023-08-20 10:31:22 +0200

    Rewrite bank scanning

--- addon/Bank.lua
+++ addon/Bank.lua
@@ -1,12 +1,32 @@
----@type LibInventory
-local _, addon = ...
-if not addon.bank then
-    return
-end
+---@type LibInventoryAce
+local addon = _G['LibInventoryAce']
 
 ---@class LibInventoryBank Manage banks which are not containers
-local lib = addon.bank
-lib.addon = addon
+local lib = addon:NewModule('LibInventoryBank', 'AceEvent-3.0')
+
+---@type LibInventoryLocations
+local inventory = addon:GetModule('LibInventoryLocations')
+
+---@type BMUtils
+local utils = _G.LibStub('BM-utils-2')
+
+function lib:OnEnable()
+    if _G['CanGuildBankRepair'] ~= nil then
+        --TODO: Find better variable to check
+        self:RegisterEvent('GUILDBANKFRAME_OPENED')
+        self:RegisterEvent('GUILDBANKFRAME_CLOSED')
+        self:RegisterEvent('GUILDBANKBAGSLOTS_CHANGED')
+    end
+
+    if _G['REAGENTBANK_CONTAINER'] ~= nil then
+        self:RegisterEvent('PLAYERREAGENTBANKSLOTS_CHANGED')
+    end
+
+    --if _G['CanUseVoidStorage'] then
+    --    self:RegisterEvent('VOID_STORAGE_OPEN')
+    --    self:RegisterEvent('VOID_STORAGE_CLOSE')
+    --end
+end
 
 function lib:scanGuildBank()
     local guildName = _G.GetGuildInfo('player');
@@ -18,10 +38,35 @@ function lib:scanGuildBank()
             local _, itemCount = _G.GetGuildBankItemInfo(tab, i);
             if itemCount > 0 then
                 local link = _G.GetGuildBankItemLink(tab, i)
-                local itemID = self.addon.utils.itemIdFromLink(link)
+                local itemID = utils.itemIdFromLink(link)
                 assert(itemID, ('Unable to get item id for slot %d'):format(i))
-                self.addon.main:saveItemLocation(itemID, 'guildBank', itemCount, guildName)
+                inventory:saveItemLocation(itemID, 'guildBank', itemCount, guildName)
             end
         end
     end
 end
+
+function lib:GUILDBANKFRAME_OPENED()
+    self.atGuildBank = true
+end
+
+function lib:GUILDBANKBAGSLOTS_CHANGED(slot)
+    --@debug@
+    utils.basic.printf('Guild Bank slot %d changed', slot)
+    --@end-debug@
+    self:scanGuildBank()
+end
+
+function lib:GUILDBANKFRAME_CLOSED()
+    self:scanGuildBank()
+    self.atGuildBank = false
+end
+
+--[[
+function events:VOID_STORAGE_OPEN()
+
+end
+
+function events:VOID_STORAGE_CLOSE()
+
+end]]
